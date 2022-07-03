9b75ffb1f0791f4d0c7bb84fd4e35f3e19ebd168
Ashwini Rathod <rashwini510@gmail.com>
2022-07-03 21:25:49 +0530

    Added: Analyzing the mood using method analyze mood with parameter message

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-    public static void main(String[] args) {
+    public static String analyzeMood(String message) {
+        if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else if (message.toLowerCase().contains("happy")) {
+            return "Happy";
+        }
+        return null;
+    }
 
-        System.out.println("Welcome to Mood analyzer program");
+    public static void main(String[] args) {
+        String mood = MoodAnalyzer.analyzeMood("User is Happy");
+        System.out.println(mood);
+        mood = MoodAnalyzer.analyzeMood("User is Sad");
+        System.out.println(mood);
     }
 }
